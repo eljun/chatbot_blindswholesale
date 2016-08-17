5d20ec0d818b19235c977069b691ff2018a3d92c
eljun <el.junsan@yahoo.com>
2016-08-17 17:14:46 +0800

    Added new cases

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,14 +1,12 @@
-var request = require('request');
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-// var json    = ('./public/json/data.json');
+var request, express, router, fs;
 
-// var token = "EAAKa7H7IXpABAB55ldmBqb08eq40RMvXZBlLw4Y6pQ95S33EDHUUMRdXaVVpWZBRmuvpZAs1ZBhE45wFZCt4aXezCofZCMV9JW0UzQ1bHs1ZCSwth0jmO1u8txEZB6C0jdu6qPev76GWxoI2F9QJITrnHdGTlchVWmK58gEZCSI9TVQZDZD";
+request = require('request');
+express = require('express');
+router  = express.Router();
+fs      = require('fs');
+
+
 // /* GET home page. */
-
-
-
 var title = "Deluxe Roller",
     image_url = "http://shoppingcart.blindswholesale.com.au/image/thumbnails/18/76/13_budget_range_roller_blind_jpg-100198-350x350.jpg",
     subtitle = "Budget Blockout is a 100% blockout fabric with...";
@@ -17,20 +15,44 @@ var title = "Deluxe Roller",
     button_price2 = "$204",
     button_title = "View item";
 
+
+function numStrip( element ) {
+    var data = element.replace(/\D/g, '');
+    return Math.floor(data);
+}
+
 router.get('/', function(req, res) {
 
     var maxWidth, minWidth, maxHeight, minHeight, width, height, dimension,  filename;
 
-    maxWidth = "";
-    minWidth = "";
+    maxWidth  = "";
+    minWidth  = "";
     maxHeight = "";
     minHeight = "";
-    width = req.query.width;
-    height = req.query.height;
+    widthT    = req.query.width;
+    width     = numStrip(req.query.width);
+    heightT   = req.query.height;
+    height    = numStrip(req.query.height);
+
+    console.log( "Not stripe" + widthT && heightT);
+    console.log( "Only accept number", width + " & the height of: " + height );
     dimension = width + "-" + height;
     filename = './json/' + dimension + '.json';
 
     switch ( dimension ) {
+        case "1000-1500":
+            createJSON({
+                dimension: dimension,
+                title: title,
+                image_url: image_url,
+                subtitle: subtitle,
+                button_link: button_link,
+                button_price: "$161.00",
+                button_title: button_title
+            });
+            res.redirect( filename );
+            break;
+
         case "1200-1700":
             createJSON({
                 dimension: dimension,
@@ -38,7 +60,7 @@ router.get('/', function(req, res) {
                 image_url: image_url,
                 subtitle: subtitle,
                 button_link: button_link,
-                button_price: button_price,
+                button_price: "$191.00",
                 button_title: button_title
             });
             res.redirect( filename );
@@ -51,7 +73,7 @@ router.get('/', function(req, res) {
                 image_url: image_url,
                 subtitle: subtitle,
                 button_link: button_link,
-                button_price: button_price,
+                button_price: "$221.00",
                 button_title: button_title
             });
             res.redirect( filename );
@@ -79,7 +101,7 @@ function invalidDimension() {
                 "template_type": "generic",
                 "elements": [{
                     "title": "Opps",
-                    "subtitle": "Incorrect dimension..",
+                    "subtitle": "Sorry dimension should be 600-3000 only.",
                     "buttons": [{
                         "type": "show_block",
                         "block_name": "Quote",
@@ -91,6 +113,7 @@ function invalidDimension() {
     }];
     return msg;
 }
+
 
 // Create new queries here
 function createJSON() {
@@ -120,17 +143,17 @@ function createJSON() {
                     "payload": {
                         "template_type": "generic",
                         "elements": [{
-                            "title": args.title,
-                            "image_url": args.image_url,
-                            "subtitle": args.subtitle,
+                            "title": a[i].title,
+                            "image_url": a[i].image_url,
+                            "subtitle": a[i].subtitle,
                             "buttons": [{
                                 "type": "web_url",
-                                "url": args.button_link,
-                                "title": args.button_price
+                                "url": a[i].button_link,
+                                "title": a[i].button_price
                             }, {
                                 "type": "web_url",
-                                "url": args.button_link,
-                                "title": args.button_title
+                                "url": a[i].button_link,
+                                "title": a[i].button_title
                             }]
                         }]
                     }
@@ -138,8 +161,9 @@ function createJSON() {
             }];
 
             var file = JSON.stringify(msg);
-            var filename = './public/json/' + args.dimension + '.json';
+            var filename = './public/json/' + a[i].dimension + '.json';
 
+            console.log( filename );
             // Store our write files here
             fs.writeFile( filename, file, 'utf8', function(err, data) {
                 if (err) {
